a9999e8db2810fd69aa3d19bea4465233b17fc0b
Enzo Marchetti <enzo.marchetti97@gmail.com>
2022-02-20 17:22:33 +0100

    Added Folders

---
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class EnemyAI : MonoBehaviour
+{
+
+    enum AIStates
+    {
+        chasing,
+        attacking
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ChasePlayer()
+    {
+
+    }
+}
