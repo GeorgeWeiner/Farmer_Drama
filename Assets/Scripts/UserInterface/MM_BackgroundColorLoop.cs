1b1b58bf2f66ae8ee0e5b2a5b7c29eabaaf33a57
George Weiner <gecovin@protonmail.com>
2022-02-20 20:07:59 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/UserInterface/MM_BackgroundColorLoop.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MM_BackgroundColorLoop : MonoBehaviour
+{
+    
+    [SerializeField] private float colorLoopSpeed;
+    
+    private float hsvValue;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        LoopThroughColors();
+    }
+
+    private void LoopThroughColors()
+    {
+        hsvValue = Mathf.MoveTowards(hsvValue, 1f, colorLoopSpeed * Time.deltaTime);
+        
+        if (hsvValue >= 0.99f)
+            hsvValue = 0f;
+
+        if (Camera.main != null) Camera.main.backgroundColor = Color.HSVToRGB(hsvValue, .7f, .7f);
+    }
+}
