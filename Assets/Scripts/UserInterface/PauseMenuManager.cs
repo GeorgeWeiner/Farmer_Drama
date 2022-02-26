596662b0eaef6e1ab819cd6fbb1037c7681162b8
Lukas P <prinz.lukas99@gmail.com>
2022-02-26 20:14:20 +0100

    lastlilpush

--- Assets/Scripts/UserInterface/PauseMenuManager.cs
+++ Assets/Scripts/UserInterface/PauseMenuManager.cs
@@ -25,7 +25,7 @@ public class PauseMenuManager : MonoBehaviour
     {
         Time.timeScale = isPaused ? 0f : 1f;
         pauseMenu.SetActive(isPaused);
-        musicPlayer.SetActive(!isPaused);
+        //musicPlayer.SetActive(!isPaused);
         isPaused = !isPaused;
         if (isPaused)
             gameMusic.Play();
